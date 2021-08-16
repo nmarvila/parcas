5630b37be6298c3080162c8fd0c1efa940444b26
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-16 18:01:41 -0300

    Módulo 23: ReactJS Conceitos Básicos do React - Finalizadas Aulas 1 e 2

---
+++ Modulo 23/react1/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// class App extends React.Component {
+
+//   render() {
+//     return <h1>Testando 1,2,3...</h1>;
+//   }
+
+// }
+
+// function App() {
+//   return <h1>Testando 3,4,5</h1>;
+// }
+
+// let App = () => {
+//   return <h1>Testando 7,8,9..</h1>;
+// };
+
+let App = () => <h1>Testando 10, 11, 12...</h1>;
+
+export default App;
