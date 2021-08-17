e5c06e8cd6c1595db600dc84628b30216e974dab
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-17 11:21:18 -0300

    Finalizada Aula 8 do Módulo 23

--- Modulo 23/react1/src/App.js
+++ Modulo 23/react1/src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const Site = styled.div`
-    width:400px;
-    height:400px;
-    background-color:#00FF00;
-`;
-
-const Botao = styled.button`
-    font-size:19px;
-    padding:10px 15px;
-    background-color: ${props => props.ativo == true ? '#0000FF' : '#CCC'};
-    color: ${props => props.ativo == true ? '#FFF' : '#000'};
-`;
 
 function App() {
+
+    const [contagem, setContagem] = useState(0);
+
+    const botaoAction = () => {
+        setContagem(contagem + 1);
+    };
+
     return (
-        <Site>
-            <Botao ativo={true}>Clique aqui</Botao>
-            <Botao ativo={false}>Clique aqui</Botao>
-        </Site>
+        <>
+            <div>{contagem} vezes</div>
+            <button onClick={botaoAction}>Clique para aumentar</button>
+        </>
     );
 }
 
