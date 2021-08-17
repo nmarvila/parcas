b95f309435b00f5c48a9c8b9cf911da1faab2aa9
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-17 10:13:18 -0300

    Finalizada Aula 3 do Módulo 23

--- Modulo 23/react1/src/App.js
+++ Modulo 23/react1/src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 
-// class App extends React.Component {
+function formatarNome(usuario) {
+    return usuario.nome + ' ' + usuario.sobrenome;
+}
 
-//   render() {
-//     return <h1>Testando 1,2,3...</h1>;
-//   }
+function App() {
+    let imagem = 'https://www.google.com.br/google.jpg';
 
-// }
-
-// function App() {
-//   return <h1>Testando 3,4,5</h1>;
-// }
-
-// let App = () => {
-//   return <h1>Testando 7,8,9..</h1>;
-// };
-
-let App = () => <h1>Testando 10, 11, 12...</h1>;
+    return <>
+        <img />
+    </>;
+}
 
 export default App;
