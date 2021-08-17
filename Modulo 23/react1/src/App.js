dabcdb2125c1f7af3411c57cce2f19c0216aa231
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-17 11:41:17 -0300

    Finalizada Aula 9 do Módulo 23

--- Modulo 23/react1/src/App.js
+++ Modulo 23/react1/src/App.js
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+const Input = styled.input`
+    width:400px;
+    height:30px;
+    font-size:16px;
+    padding:10px;
+    border:1px solid #000;
+`;
 
 function App() {
 
-    const [contagem, setContagem] = useState(0);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const botaoAction = () => {
-        setContagem(contagem + 1);
-    };
+    const handleButton = () => {
+        alert(email + ' - ' + password);
+    }
 
     return (
         <>
-            <div>{contagem} vezes</div>
-            <button onClick={botaoAction}>Clique para aumentar</button>
+            <Input placeholder="Digite um e-mail" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <Input placeholder="Digite uma senha" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleButton}>Dizer</button>
         </>
     );
 }
