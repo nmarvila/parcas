2d5ced2fd25713b13aeacf50cd34b3fb6de5a731
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-19 10:24:20 -0300

    Finalizada Aula 19 do Módulo 23

--- Modulo 23/react1/src/App.js
+++ Modulo 23/react1/src/App.js
@@ -1,62 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import SearchBox from './components/SearchBox';
 
 function App() {
 
-    const [searchText, setSearchText] = useState('');
-    const [list, setList] = useState([]);
+    const [name, setName] = useState(localStorage.getItem('name'));
 
     useEffect(() => {
-
-        setList([
-            { title: 'Comprar o bolo', done: false },
-            { title: 'Pegar o cachorro no Petshop', done: true },
-            { title: 'Gravar aula', done: false }
-        ]);
-
-    }, []);
-
-    function addAction(newItem) {
-        let newList = [...list];
-
-        newList.push({ title: newItem, done: false });
-
-        setList(newList);
-    }
-
-    function handleToggleDone(index) {
-        let newList = [...list];
-
-        newList[index].done = !newList[index].done;
-
-        setList(newList);
-    }
+        localStorage.setItem('name', name);
+    }, [name]);
 
     return (
         <>
-            <h1>Lista de Tarefas</h1>
-
-            <SearchBox
-                frasePadrao="Adicione um item"
-                onEnter={addAction}
-            />
-
-            <hr />
-
-            <ul>
-                {list.map((item, index) => (
-                    <li key={index} onClick={() => handleToggleDone(index)}>
-                        {item.done &&
-                            <del>{item.title}</del>
-                        }
-                        {!item.done &&
-                            item.title
-                        }
-                    </li>
-                ))}
-            </ul>
-
+            <input type="text" value={name} onChange={e => setName(e.target.value)} />
         </>
     );
 }
--- Modulo 23/react1/src/components/SearchBox.js
+++ Modulo 23/react1/src/components/SearchBox.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const InputText = styled.input`
@@ -14,7 +14,7 @@ function SearchBox(props) {
     const [texto, setTexto] = useState('');
 
     function handleKeyUp(e) {
-        if (e.keyCode == 13) {
+        if (e.keyCode === 13) {
             if (props.onEnter) {
                 props.onEnter(texto);
             }
