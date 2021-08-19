f50e727cc4d6813478a2313b9a0b6039da92aa03
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-19 17:44:34 -0300

    Finalizada Aula 3 do Módulo 24

--- Modulos 23 e 24/react1/src/index.js
+++ Modulos 23 e 24/react1/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Reducers from './reducers';
 import App from './App';
 
+const store = createStore(Reducers);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
