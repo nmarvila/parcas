66b96250e0df4d00a61976eb9d194e4606a0d9f0
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-19 11:27:55 -0300

    Finalizada Aula 22 do Módulo 23

--- Modulo 23/react1/src/App.js
+++ Modulo 23/react1/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Categoria from "./pages/Categoria";
 
 function App() {
 
@@ -15,9 +18,40 @@ function App() {
                         <li>
                             <Link to="/sobre">Sobre</Link>
                         </li>
+                        <li>
+                            <Link to="/categoria/esportes">Esportes</Link>
+                        </li>
+                        <li>
+                            <Link to="/categoria/noticias">Notícias</Link>
+                        </li>
+                        <li>
+                            <Link to="/categoria/viagem">Viagem</Link>
+                        </li>
                     </ul>
                 </nav>
             </header>
+            <hr />
+
+            <Switch>
+
+                <Route exact path="/">
+                    <Home />
+                </Route>
+
+                <Route path="/sobre">
+                    <Sobre />
+                </Route>
+
+                <Route path="/categoria/:cat">
+                    <Categoria />
+                </Route>
+
+            </Switch>
+
+            <hr />
+            <footer>
+                Todos os direitos reservados...
+            </footer>
         </BrowserRouter>
     );
 }
---
+++ Modulo 23/react1/src/pages/Sobre.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Sobre() {
+
+    return (
+        <div>
+            <h4>Página SOBRE</h4>
+            Bla bla bla blab la
+        </div>
+    );
+}
+
+export default Sobre;
