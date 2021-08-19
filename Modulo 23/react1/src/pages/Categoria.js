0837dbd234775c72727c1f9245a49e29a193f122
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-19 11:36:40 -0300

    Finalizada Aula 23 do Módulo 23

--- Modulo 23/react1/src/pages/Categoria.js
+++ Modulo 23/react1/src/pages/Categoria.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
 
 function Categoria() {
+    let query = useQuery();
 
-    let { cat } = useParams();
+    let cat = query.get('tipo');
+    let subtipo = query.get('subtipo');
 
     return (
         <div>
             <h4>Página Categoria</h4>
-            Exibindo itens da categoria: {cat}
+            Exibindo itens da categoria: {cat} - {subtipo}
         </div>
     );
 }
