a1b823e402472b991cbe606f112be093ef20c0aa
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-23 10:09:52 -0300

    Finalizada Aula 5 do Módulo 24

--- Modulos 23 e 24/react1/src/pages/Login.js
+++ Modulos 23 e 24/react1/src/pages/Login.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-function Login() {
+function Login(props) {
+
+    const handleFelipe = () => {
+        props.setName('Felipe');
+    };
 
     return (
         <div>
             <h4>Página LOGIN</h4>
+
+            O nome é: {props.name}
+
+            <br /><br />
+            <button onClick={handleFelipe}>Trocar nome para Felipe</button>
         </div>
     );
 }
 
-export default Login;
+const mapStateToProps = state => ({
+    name: state.usuario.name
+});
+
+const mapDispatchToProps = dispatch => ({
+    setName: (newName) => dispatch({
+        type: 'SET_NAME',
+        payload: { name: newName }
+    })
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
