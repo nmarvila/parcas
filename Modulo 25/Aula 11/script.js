c49cdd9f3d6bab8e0bdddec34aa2970238f94bc4
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-23 17:48:11 -0300

    Finalizada Aula 11 do Módulo 25

---
+++ Modulo 25/Aula 11/script.js
@@ -0,0 +1,43 @@
+class Pessoa extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            nome: this.props.nome
+        };
+
+        this.limparNome = this.limparNome.bind(this);
+        this.setNome = this.setNome.bind(this);
+    }
+
+    limparNome() {
+        this.setState({ nome: '' });
+    }
+
+    setNome(nome) {
+        this.setState({ nome });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Olá, meu nome é {this.state.nome}</h1>
+                <button onClick={this.limparNome}>Limpar Nome</button>
+                <button onClick={() => { this.setNome('João') }}>Mudar nome para João</button>
+            </div>
+        );
+    }
+
+}
+
+
+let elemento = (
+    <div>
+        <Pessoa nome="Nichollas" />
+    </div>
+);
+
+ReactDOM.render(
+    elemento,
+    document.getElementById("app")
+);
