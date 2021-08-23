fcb133a3967789c2185d3cc9296f5ec4da6fe5ee
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-23 11:48:02 -0300

    Finalizada Aula 5 do Módulo 25

---
+++ Modulo 25/Aula 5/script.js
@@ -0,0 +1,16 @@
+// let elemento = <h1 className="titulo">Olá Mundo</h1>;
+
+/*
+let elemento = React.createElement(
+    'h1',
+    { className: 'titulo' },
+    'Olá Mundo 2.0'
+);
+*/
+
+let elemento = <h1 className="titulo">Olá Mundo 3.0</h1>;
+
+ReactDOM.render(
+    elemento,
+    document.getElementById("app")
+);
