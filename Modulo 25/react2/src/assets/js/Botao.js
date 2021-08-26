e06230e06354dea54f23a43adee5b93986185b89
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-26 17:02:29 -0300

    Finalizada Aula 27 do Módulo 25

---
+++ Modulo 25/react2/src/assets/js/Botao.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Botao extends Component {
+
+    render() {
+        return (
+            <button>Clique aqui</button>
+        );
+    }
+}
+
+export default Botao;
