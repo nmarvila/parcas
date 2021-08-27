8b72a9785d1f6e48e436c66eac76fac0902849b2
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-27 10:51:49 -0300

    Finalizada Aula 36 do Módulo 25

--- Modulo 25/react2/src/pages/Home/Home.js
+++ Modulo 25/react2/src/pages/Home/Home.js
@@ -3,10 +3,41 @@ import { Link } from 'react-router-dom';
 
 export class Home extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            ip: ''
+        };
+
+    }
+
+    componentDidMount() {
+
+        fetch('https://api.ipify.org/?format=json')
+            .then((r) => {
+                return r.json()
+            })
+            .then((json) => {
+
+                this.setState({ ip: json.ip });
+
+            });
+
+    }
+
     render() {
         return (
             <div>
-                Página Home...
+                <h3>Página Home</h3>
+
+                <div>
+                    {this.state.ip == '' &&
+                        <i>Carregando...</i>
+                    }
+                    {this.state.ip != '' &&
+                        <p>Seu IP é: {this.state.ip}</p>
+                    }
+                </div>
 
                 <Link to="/sobre">Ir para página sobre</Link>
             </div>
