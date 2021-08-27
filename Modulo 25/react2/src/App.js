ad1f2b25d74a1545f198cda85db27047f69e92fc
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-27 09:53:01 -0300

    Finalizada Aula 32 do Módulo 25

---
+++ Modulo 25/react2/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import { Header } from './components/Header';
+
+import { Home } from './pages/Home';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <Header />
+
+                <Home />
+            </div>
+        );
+    }
+
+}
+
+export default App;
