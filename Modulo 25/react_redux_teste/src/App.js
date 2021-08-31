1e4cfa0f2b270c1c42677b6284b20b31be1d6592
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-31 17:24:49 -0300

    Finalizada Aula 50 do Módulo 25

--- Modulo 25/react_redux_teste/src/App.js
+++ Modulo 25/react_redux_teste/src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import Reducers from './Reducers';
+
 import Config from './Config';
 
 import { Header } from './components/Header';
 import { Sobre } from './pages/Sobre';
 import { Home } from './pages/Home';
 
+const store = createStore(Reducers);
 
 class App extends Component {
 
     render() {
         return (
-            <BrowserRouter basename={Config.BASE_URL}>
-                <div>
-                    <Header />
+            <Provider store={store}>
+                <BrowserRouter basename={Config.BASE_URL}>
+                    <div>
+                        <Header />
 
-                    <Route exact path="/" component={Home} />
-                    <Route path="/sobre" component={Sobre} />
-                </div>
-            </BrowserRouter>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/sobre" component={Sobre} />
+                    </div>
+                </BrowserRouter>
+            </Provider>
         );
     }
 
