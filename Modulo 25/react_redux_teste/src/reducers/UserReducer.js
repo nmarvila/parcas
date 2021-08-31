7ac029df0b15cb10eff29c0d904bdb07a081f152
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-31 17:17:46 -0300

    Finalizada Aula 49 do Módulo 25

---
+++ Modulo 25/react_redux_teste/src/reducers/UserReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    email: '',
+    pass: ''
+};
+
+const UserReducer = (state = initialState, action) => {
+
+    // actions específicas ficam aqui.
+
+    return state;
+};
+
+export default UserReducer;
