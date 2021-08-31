56321e7b181bf080a763dad5160ab273f102cc40
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-08-31 12:08:54 -0300

    Finalizada Aula 43 do Módulo 25

--- Modulo 25/react_login/src/pages/Login/Login.js
+++ Modulo 25/react_login/src/pages/Login/Login.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
+import CSS from './Login.css'
 
 export class Login extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            errorMsg: ''
+        };
+
+        this.entrar = this.entrar.bind(this);
+    }
+
+    entrar() {
+        let email = document.getElementById('email').value;
+        let pass = document.getElementById('pass').value;
+
+        fetch('https://alunos.b7web.com.br/apis/loginteste/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, pass })
+        })
+            .then(r => r.json())
+            .then(json => {
+
+                this.setState({ errorMsg: json.error });
+
+                console.log(json);
+
+            });
+    }
+
     render() {
         return (
-            <div>
-                Página de Login
+            <div class="loginarea">
+
+                <h1>Login</h1>
+
+                <p class="error">{this.state.errorMsg}</p>
+
+                <label>
+                    E-mail:
+                    <br />
+                    <input type="email" id="email" />
+                </label>
+
+                <label>
+                    Senha:
+                    <br />
+                    <input type="password" id="pass" />
+                </label>
+
+                <button onClick={this.entrar} >Entrar</button>
+
             </div>
         );
     }
