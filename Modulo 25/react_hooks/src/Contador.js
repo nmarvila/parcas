c9a3d9177222e545ea20772345847c82b85cd41c
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-09-01 16:28:31 -0300

    Finalizada Aula 64 do Módulo 25

--- Modulo 25/react_hooks/src/Contador.js
+++ Modulo 25/react_hooks/src/Contador.js
@@ -4,8 +4,21 @@ const initialState = {
     contagem: 0
 };
 
-const reducer = () => {
-
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'add_plus':
+            return { ...state, contagem: state.contagem + action.qt };
+            break;
+        case 'add':
+            return { ...state, contagem: state.contagem + 1 };
+            break;
+        case 'minus':
+            return { ...state, contagem: state.contagem - 1 };
+            break;
+        case 'reset':
+            return initialState;
+            break;
+    }
 };
 
 const Contador = () => {
@@ -16,9 +29,10 @@ const Contador = () => {
         <div>
             <h4>Contagem: {state.contagem}</h4>
 
-            <button>+</button>
-            <button>-</button>
-            <button>reset</button>
+            <button onClick={() => dispatch({ type: 'add_plus', qt: 10 })}>+10</button>
+            <button onClick={() => dispatch({ type: 'add' })}>+</button>
+            <button onClick={() => dispatch({ type: 'minus' })}>-</button>
+            <button onClick={() => dispatch({ type: 'reset' })}>reset</button>
         </div>
     );
 };
