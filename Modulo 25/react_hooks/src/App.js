78fa39cfaf7965e5f3ff2db5e3532bea1d49d8b0
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-09-01 11:39:14 -0300

    Finalizada Aula 59 do Módulo 25

--- Modulo 25/react_hooks/src/App.js
+++ Modulo 25/react_hooks/src/App.js
@@ -1,23 +1,31 @@
 import React, { useState } from 'react';
 import './App.css';
+import formSubmit from './formSubmit';
 
-export default () => {
+const App = () => {
 
-  const [msg, setMsg] = useState('Mostrar');
-  const [mostrar, setMostrar] = useState(false);
+  // const [valor, setValor] = useState('');
+  const [email, setEmail] = useState('teste@hotmail.com');
 
-  const handleButtonClick = () => {
-    setMostrar(!mostrar);
-    setMsg(mostrar ? 'Mostrar' : 'Ocultar');
-  };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    formSubmit(email, setEmail);
+  }
 
   return (
     <div>
-      <button onClick={handleButtonClick}>{msg}</button>
-      {mostrar &&
-        <h2>Men$4gem S3cr3t4!!! 2.0</h2>
-      }
+
+      <h1>Bem Vindo(a)</h1>
+
+      <form onSubmit={handleFormSubmit}>
+        <label>Qual Seu E-mail?</label>
+        <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+
+        <input className="submit" type="submit" value="Enviar" />
+      </form>
+
     </div>
   );
+};
 
-}
+export default App;
---
+++ Modulo 25/react_hooks/src/formSubmit.js
@@ -0,0 +1,8 @@
+const formSubmit = (email, setEmail) => {
+
+    console.log("E-mail enviado para " + email + "!");
+    setEmail('');
+
+};
+
+export default formSubmit;
