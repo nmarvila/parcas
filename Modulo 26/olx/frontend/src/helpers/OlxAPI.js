47f24c3c5ddd02a9a1fe5be7b44684a25a3e4959
Nichollas Marvila <nichollasmarvila@gmail.com>
2021-10-31 23:32:32 -0300

    Finalizadas Aulas 1-15 do Módulo 26

---
+++ Modulo 26/olx/frontend/src/helpers/OlxAPI.js
@@ -0,0 +1,12 @@
+
+const OlxAPI = {
+
+    login: async (email, password) => {
+        // fazer consulta ao WB
+
+        return { error: 'Funcionalidade Incompleta' };
+    }
+
+};
+
+export default () => OlxAPI;
